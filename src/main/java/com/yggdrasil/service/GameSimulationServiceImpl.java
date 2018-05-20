e77309655eeed3c54b698a98db832e2ebda227d1
OlegGasiul <oleg.gasiul@informa.com>
2018-05-20 21:09:35 +0300

    Refactoring

--- src/main/java/com/yggdrasil/service/GameSimulationServiceImpl.java
+++ src/main/java/com/yggdrasil/service/GameSimulationServiceImpl.java
@@ -3,9 +3,15 @@ package com.yggdrasil.service;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
 import java.math.BigDecimal;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.concurrent.CompletableFuture;
-import java.util.stream.IntStream;
+import java.util.function.Supplier;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 @Slf4j
 @Service
@@ -15,22 +21,20 @@ public class GameSimulationServiceImpl implements GameSimulationService {
     private GameSimulationRepeaterFactory gameSimulationRepeaterFactory;
 
     public BigDecimal calculateAverageAward(long attempts, int threads) {
-        CompletableFuture<BigDecimal>[] futures = new CompletableFuture[threads];
         long repeats = attempts / threads;
-        IntStream.range(0, threads).forEach(i -> {
-            GameSimulationRepeater supplier = gameSimulationRepeaterFactory.createSupplier(repeats);
-            futures[i] = CompletableFuture.supplyAsync(supplier);
-        });
+
+        Supplier<List<CompletableFuture<BigDecimal>>> supplier = () -> new LinkedList<>();
+        CompletableFuture<BigDecimal>[] futures = Stream.generate(() -> CompletableFuture.supplyAsync(gameSimulationRepeaterFactory.createSupplier(repeats)))
+                .limit(threads)
+                .collect(Collectors.toCollection(supplier)).toArray(new CompletableFuture[threads]);
 
         try {
             CompletableFuture.allOf(futures).join();
 
-            BigDecimal result = BigDecimal.ZERO;
-            for (CompletableFuture<BigDecimal> future : futures) {
-                result = result.add(future.get());
-            }
-
-            return result.divide(BigDecimal.valueOf(threads));
+            return Arrays.stream(futures)
+                .map((future) -> future.getNow(BigDecimal.ZERO))
+                .reduce(BigDecimal.ZERO, BigDecimal::add)
+                .divide(BigDecimal.valueOf(threads));
         } catch (Exception e) {
             log.error(e.getMessage(), e);
             return null;
