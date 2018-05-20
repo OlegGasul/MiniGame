9870fb31acc34e4fba55cc4818325c09014a9dd3
OlegGasiul <oleg.gasiul@informa.com>
2018-05-20 17:18:55 +0300

    Fixed plugins

--- src/main/java/com/yggdrasil/service/GameSimulationServiceImpl.java
+++ src/main/java/com/yggdrasil/service/GameSimulationServiceImpl.java
@@ -15,7 +15,7 @@ public class GameSimulationServiceImpl implements GameSimulationService {
     private GameSimulationSupplierFactory gameSimulationSupplierFactory;
 
     public BigDecimal calculateAverageAward(long attempts, int threads) {
-        CompletableFuture[] futures = new CompletableFuture[threads];
+        CompletableFuture<Double>[] futures = new CompletableFuture[threads];
         IntStream.range(0, threads).forEach(i -> {
             GameSimulationRepeater supplier = gameSimulationSupplierFactory.createSupplier(attempts / threads);
             futures[i] = CompletableFuture.supplyAsync(supplier);
@@ -25,8 +25,8 @@ public class GameSimulationServiceImpl implements GameSimulationService {
             CompletableFuture.allOf(futures).join();
 
             BigDecimal result = BigDecimal.ZERO;
-            for (CompletableFuture future : futures) {
-                result = result.add(BigDecimal.valueOf((Double) future.get()));
+            for (CompletableFuture<Double> future : futures) {
+                result = result.add(BigDecimal.valueOf(future.get()));
             }
 
             return result.divide(BigDecimal.valueOf(threads));
