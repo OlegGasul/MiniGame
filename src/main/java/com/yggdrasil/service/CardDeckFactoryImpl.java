f7d0b132a32fd08ca4a6dc8c41b06a505e96e43d
OlegGasiul <oleg.gasiul@informa.com>
2018-05-22 00:17:32 +0300

    Refactoring: changed shuffling algorithm

--- src/main/java/com/yggdrasil/service/CardDeckFactoryImpl.java
+++ src/main/java/com/yggdrasil/service/CardDeckFactoryImpl.java
@@ -2,29 +2,36 @@ package com.yggdrasil.service;
 
 import com.yggdrasil.entity.Card;
 import org.springframework.stereotype.Service;
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collections;
+import java.util.Collection;
+import java.util.concurrent.ThreadLocalRandom;
 
 import static com.yggdrasil.entity.Card.*;
 
 @Service
 public class CardDeckFactoryImpl implements CardDeckFactory {
-    public ArrayList<Card> generateCards() {
-        ArrayList<Card> cards = new ArrayList<>(Arrays.asList(EUR_100,
+    public Collection<Card> generateCards() {
+        return shuffle(Arrays.asList(EUR_100,
                 EUR_20, EUR_20,
                 EUR_5, EUR_5, EUR_5, EUR_5, EUR_5,
                 EXTRA_LIFE,
                 GAME_OVER, GAME_OVER, GAME_OVER));
-        Collections.shuffle(cards);
-
-        return cards;
     }
 
-    public ArrayList<Card> generateAdditionalCards() {
-        ArrayList<Card> cards = new ArrayList<>(Arrays.asList(EUR_5, EUR_10, EUR_20, SECOND_CHANCE));
-        Collections.shuffle(cards);
+    public Collection<Card> generateAdditionalCards() {
+        return shuffle(Arrays.asList(EUR_5, EUR_10, EUR_20, SECOND_CHANCE));
+    }
 
-        return cards;
+    private Collection<Card> shuffle(Collection<Card> cards) {
+        Card[] arr = cards.toArray(new Card[cards.size()]);
+        for (int i = arr.length - 1; i >= 1; i--) {
+            int j = ThreadLocalRandom.current().nextInt(0, i);
+
+            Card klop = arr[j];
+            arr[j] = arr[i];
+            arr[i] = klop;
+        }
+
+        return Arrays.asList(arr);
     }
 }
